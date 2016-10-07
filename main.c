12196de86b77597033a8e7b57f79a7a39b8eaf28
Natalisa <nnsuhodoeva@mail.ru>
2016-10-07 20:46:15 +0700

    Косметические изменения

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <dirent.h>
 
@@ -15,7 +16,7 @@ struct files_inf{
   int type;
 }mass[250];
 //char mass[250][250];
-int i=0,w=0,j;
+int i = 0, w = 0, j;
 
 void sig_winch(int signo){
   struct winsize size;
@@ -25,17 +26,17 @@ void sig_winch(int signo){
 
 void dir(char *path){//Считывание дериктории
   FILE *f = fopen("test.txt","w");
-  i=0;
+  i = 0;
   DIR *fd = opendir(path);
   struct dirent *q;
-  for(j=0;j<250;j++){
+  for(j = 0;j < 250; j++){
     strcpy(mass[j].name, " ");
   }
   while((q = readdir(fd)) != NULL){
-    if(strcmp(q->d_name,".")!=0){
+    if(strcmp(q->d_name,".") != 0){
       strcpy(mass[i].name, q->d_name);
       mass[i].type = q->d_type;
-      fprintf(f,"%s %d\n",mass[i].name,mass[i].type );
+      fprintf(f,"%s %d\n", mass[i].name, mass[i].type );
       i++;
     }
   }
@@ -65,7 +66,7 @@ int main(int argc, char **argv){
   // box(leftwnd,'|','-');
   scrollok(leftwnd,true);
   dir("./");
-  for(j=i-1;j>=0;j--){
+  for(j = i - 1; j >= 0; j--){
     wprintw(leftwnd," %s\n",mass[j].name);
   }
 
@@ -73,7 +74,7 @@ int main(int argc, char **argv){
   rightwnd = derwin(wnd, size.ws_row, (int)size.ws_col/2, 0, (int)size.ws_col/2);
   // box(rightwnd,'|','-');
   scrollok(rightwnd,true);
-  for(j=i-1;j>=0;j--){
+  for(j = i - 1; j >= 0; j--){
     wprintw(rightwnd," %s\n",mass[j].name);
   }
 
@@ -83,20 +84,20 @@ int main(int argc, char **argv){
 
   WINDOW *activwnd=leftwnd;
   int col = 0, row = 0;
-  int temp,activ=1,nstr=i-1;
+  int temp, activ = 1, nstr = i - 1;
 
   char path[512],pathleft[512] = "./",pathright[512] = "./";
   strcpy(path,pathleft);
 //СЧИТЫВАНИЕ
   while(true){
     wclear(activwnd);
-    for(j=i-1;j>=0;j--){
-      if(j==nstr){
+    for(j = i-1;j >=0 ; j--){
+      if(j == nstr){
         wattroff(activwnd,COLOR_PAIR(0));
         wattron(activwnd,COLOR_PAIR(1));
       }
       wprintw(activwnd," %s\n",mass[j].name);
-      if(j==nstr){
+      if(j == nstr){
         wattroff(activwnd,COLOR_PAIR(1));
         wattron(activwnd,COLOR_PAIR(0));
       }
@@ -138,10 +139,10 @@ int main(int argc, char **argv){
               } else { //все остальные(редактор)
                 switch(fork()){
                   case 0:
-                    execlp("../editor/1","1",NULL);
+                    execlp("../editor/editor","editor",mass[nstr].name,NULL);
                   break;
                   default:
-                    wait(0);
+                    wait(NULL);
                   break;
                 }
               }
@@ -160,10 +161,10 @@ int main(int argc, char **argv){
           activwnd=leftwnd;
           strcpy(pathright,path);
           strcpy(path,pathleft);
-          activ=1;
+          activ = 1;
         }
         dir(path);
-        nstr=i-1;
+        nstr = i - 1;
        }; break;
 
       case KEY_UP: {
